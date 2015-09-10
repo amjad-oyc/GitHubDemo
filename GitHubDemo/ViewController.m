0801a7ec46788457696f93f6336028d53eadfb4e
Amjad <amjad@Amjads-MacBook-Air.local>
2015-09-10 14:50:20 +0530

    added tutorial link in project.

--- GitHubDemo/ViewController.m
+++ GitHubDemo/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Afycon. All rights reserved.
 //
 
+// Setup Xcode Projects with Github https://www.youtube.com/watch?v=2U_84-vg8OE
+
 #import "ViewController.h"
 
 @interface ViewController ()
