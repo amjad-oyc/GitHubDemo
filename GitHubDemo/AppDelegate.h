e571f70e5ba5338367c28e4daea66e88615a3726
Amjad <amjad@Amjads-MacBook-Air.local>
2015-09-10 13:27:55 +0530

    Initial Commit

---
+++ GitHubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubDemo
+//
+//  Created by Amjad on 9/10/15.
+//  Copyright (c) 2015 Afycon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubDemo
+//
+//  Created by Amjad on 9/10/15.
+//  Copyright (c) 2015 Afycon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
